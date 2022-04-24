8a055cfea19dc34c2faa036cdfc4890d24670755
AleksandrMatsko <a.matsko@g.nsu.ru>
2022-04-24 20:15:23 +0700

    try to include MPI

--- task4/main.cpp
+++ task4/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <mpi.h>
+#include <malloc.h>
 
 int NUM_LINES;
 int NUM_COLUMNS;
@@ -39,28 +41,36 @@ void init(u_int *stage) {
 
 }
 
-void calcNextStage(u_int *stage, u_int *next_stage, int num_lines) {
-    for (int i = 0; i < num_lines; i++) {
+void calcNextStageCenter(u_int *stage, u_int *next_stage, int num_lines) {
+    for (int i = 1; i < num_lines; i++) {
         for (int j = 0; j < NUM_COLUMNS; j++) {
+            /*
+             *      order of checking neighbours
+             *
+             *      3 4 5
+             *      2 c 1
+             *      6 7 8
+             */
             u_int num_alive = 0;
             num_alive += getBit(stage[i * NUM_UINTS + (j + NUM_COLUMNS + 1) % NUM_COLUMNS / UINT_BIT_SIZE],
                                 (j + 1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
             num_alive += getBit(stage[i * NUM_UINTS + (j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
                                 (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
 
-            num_alive += getBit(stage[((i + NUM_LINES - 1) % NUM_LINES) * NUM_UINTS + (j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+            num_alive += getBit(stage[(i - 1) * NUM_UINTS + (j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
                                 (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
-            num_alive += getBit(stage[((i + NUM_LINES - 1) % NUM_LINES) * NUM_UINTS + j / UINT_BIT_SIZE],
+            num_alive += getBit(stage[(i - 1) * NUM_UINTS + j / UINT_BIT_SIZE],
                                 j % UINT_BIT_SIZE);
-            num_alive += getBit(stage[((i + NUM_LINES - 1) % NUM_LINES) * NUM_UINTS + (j + 1) / UINT_BIT_SIZE],
+            num_alive += getBit(stage[(i - 1) * NUM_UINTS + (j + 1) / UINT_BIT_SIZE],
                                 (j + 1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
 
-            num_alive += getBit(stage[((i + 1) % NUM_LINES) * NUM_UINTS + (j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+            num_alive += getBit(stage[(i + 1) * NUM_UINTS + (j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
                                 (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
-            num_alive += getBit(stage[((i + 1) % NUM_LINES) * NUM_UINTS + j / UINT_BIT_SIZE],
+            num_alive += getBit(stage[(i + 1) * NUM_UINTS + j / UINT_BIT_SIZE],
                                 j % UINT_BIT_SIZE);
-            num_alive += getBit(stage[((i + 1) % NUM_LINES) * NUM_UINTS + (j + 1) / UINT_BIT_SIZE],
+            num_alive += getBit(stage[(i + 1) * NUM_UINTS + (j + 1) / UINT_BIT_SIZE],
                                 (j +  1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
+
             u_int bit = getBit(stage[i * NUM_UINTS + j / UINT_BIT_SIZE], j % UINT_BIT_SIZE);
             if ((num_alive < 2 || num_alive > 3) && bit == 1) {
                 next_stage[i * NUM_UINTS + j / UINT_BIT_SIZE] = setBit(next_stage[i * NUM_UINTS + j / UINT_BIT_SIZE], j % UINT_BIT_SIZE, 0);
@@ -74,6 +84,77 @@ void calcNextStage(u_int *stage, u_int *next_stage, int num_lines) {
         }
     }
 }
+
+void calcNextStageUp(u_int *stage, u_int *next_stage, int num_lines, u_int *cells_up) {
+    for (int j = 0; j < NUM_COLUMNS; j++) {
+        u_int num_alive = 0;
+        num_alive += getBit(stage[(j + NUM_COLUMNS + 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + 1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
+        num_alive += getBit(stage[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+
+        num_alive += getBit(cells_up[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+        num_alive += getBit(cells_up[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+        num_alive += getBit(cells_up[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j +  1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
+
+        num_alive += getBit(stage[NUM_UINTS + (j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+        num_alive += getBit(stage[NUM_UINTS + j / UINT_BIT_SIZE],
+                            j % UINT_BIT_SIZE);
+        num_alive += getBit(stage[NUM_UINTS + (j + 1) / UINT_BIT_SIZE],
+                            j % UINT_BIT_SIZE);
+
+        u_int bit = getBit(stage[j / UINT_BIT_SIZE], j % UINT_BIT_SIZE);
+        if ((num_alive < 2 || num_alive > 3) && bit == 1) {
+            next_stage[j / UINT_BIT_SIZE] = setBit(next_stage[j / UINT_BIT_SIZE], j % UINT_BIT_SIZE, 0);
+        }
+        else if (num_alive == 3 && bit == 0) {
+            next_stage[j / UINT_BIT_SIZE] = setBit(next_stage[j / UINT_BIT_SIZE], j % UINT_BIT_SIZE, 1);
+        }
+        else {
+            next_stage[j / UINT_BIT_SIZE] = setBit(next_stage[j / UINT_BIT_SIZE], j % UINT_BIT_SIZE, bit);
+        }
+    }
+}
+
+void calcNextStageDown(u_int *stage, u_int *next_stage, int num_lines, u_int *cells_down) {
+    for (int j = 0; j < NUM_COLUMNS; j++) {
+        u_int num_alive = 0;
+        num_alive += getBit(stage[(j + NUM_COLUMNS + 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + 1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
+        num_alive += getBit(stage[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+
+        num_alive += getBit(stage[(num_lines - 1) * NUM_UINTS + (j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+        num_alive += getBit(stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE],
+                            j % UINT_BIT_SIZE);
+        num_alive += getBit(stage[(num_lines - 1) * NUM_UINTS + (j + 1) / UINT_BIT_SIZE],
+                            (j + 1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
+
+        num_alive += getBit(cells_down[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+        num_alive += getBit(cells_down[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j + UINT_BIT_SIZE - 1 + (!j) * NUM_COLUMNS) % UINT_BIT_SIZE);
+        num_alive += getBit(cells_down[(j + NUM_COLUMNS - 1) % NUM_COLUMNS / UINT_BIT_SIZE],
+                            (j +  1) * (j != NUM_COLUMNS - 1) % UINT_BIT_SIZE);
+
+        u_int bit = getBit(stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE], j % UINT_BIT_SIZE);
+        if ((num_alive < 2 || num_alive > 3) && bit == 1) {
+            next_stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE] = setBit(next_stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE], j % UINT_BIT_SIZE, 0);
+        }
+        else if (num_alive == 3 && bit == 0) {
+            next_stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE] = setBit(next_stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE], j % UINT_BIT_SIZE, 1);
+        }
+        else {
+            next_stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE] = setBit(next_stage[(num_lines - 1) * NUM_UINTS + j / UINT_BIT_SIZE], j % UINT_BIT_SIZE, bit);
+        }
+    }
+}
+
 
 bool checkPrevStage(u_int *prev_stage, u_int *stage, int num_lines) {
     for (int i = 0; i < num_lines; i++) {
@@ -91,16 +172,61 @@ int main(int argc, char *argv[]) {
         std::cerr << "Wrong amount of arguments: expected 3" << std::endl;
         return 1;
     }
+
+    int size, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     NUM_LINES = atoi(argv[1]);
     NUM_COLUMNS = atoi(argv[2]);
     NUM_UINTS = NUM_COLUMNS / (UINT_BIT_SIZE) + (NUM_COLUMNS % (UINT_BIT_SIZE) != 0);
+
+    int *strings_in_tasks = (int *)calloc(size, sizeof(int));
+    int *shifts = (int *) calloc(size, sizeof(int));
+
+    int num_of_strings = NUM_LINES / size;
+    int remainder = NUM_LINES % size;
+    for (int i = 0; i < size; i++) {
+        strings_in_tasks[i] = num_of_strings;
+        if (i < remainder) {
+            strings_in_tasks[i] += 1;
+        }
+    }
+
+    int start_index = 0;
+    for (int i = 0; i < size; i++) {
+        shifts[i] = start_index;
+        start_index += strings_in_tasks[i] * NUM_UINTS;
+    }
+
+    int *to_send = (int *)calloc(size, sizeof(int));
+    for (int i = 0; i < size; i++) {
+        to_send[i] = NUM_UINTS * strings_in_tasks[i];
+    }
+
     u_int *prev_stages[MAX_ITERATION];
-    int current_iteration = 0;
-    size_t field_size = NUM_LINES * NUM_UINTS;
+    size_t full_field_size = NUM_LINES * NUM_UINTS;
+    size_t field_size = strings_in_tasks[rank] * NUM_UINTS;
     u_int *stage = (u_int *)calloc(field_size, sizeof(u_int));
     u_int *next_stage = (u_int *)calloc(field_size, sizeof(u_int));
 
-    init(stage);
+
+    double start_time = MPI_Wtime();
+    u_int *full_field;
+    if (rank == 0 ) {
+        full_field = (u_int *)calloc(full_field_size, sizeof(u_int));
+        init(full_field);
+    }
+
+    MPI_Scatterv(full_field, to_send, shifts, MPI_UNSIGNED, stage, NUM_UINTS * strings_in_tasks[rank], MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        free(full_field);
+    }
+
+    u_int *cells_up = (u_int *)calloc(NUM_UINTS, sizeof(u_int));
+    u_int *cells_down = (u_int *)calloc(NUM_UINTS, sizeof(u_int));
+
     /*for (int i = 0; i < NUM_LINES; i++) {
         for (int j = 0; j < NUM_UINTS; j++) {
             printUINT(stage[i * NUM_UINTS + j]);
@@ -108,11 +234,26 @@ int main(int argc, char *argv[]) {
         std::cout << std::endl;
     }*/
     std::cout << std::endl;
-    bool period = false;
-    while (current_iteration < MAX_ITERATION && !period) {
+    int current_iteration = 0;
+    int is_global_period = false;
+    while (current_iteration < MAX_ITERATION && !is_global_period) {
+        MPI_Request req_recv_up, req_recv_down;
+        MPI_Irecv(cells_up, NUM_UINTS, MPI_UNSIGNED, (rank - 1 + size) % size, 0, MPI_COMM_WORLD, &req_recv_up);
+        MPI_Irecv(cells_down, NUM_UINTS, MPI_UNSIGNED, (rank + 1) % size, 0, MPI_COMM_WORLD, &req_recv_down);
+
+        MPI_Request req_send[2];
+        MPI_Isend(stage, NUM_UINTS, MPI_UNSIGNED, (rank - 1 + size) % size, 0, MPI_COMM_WORLD, req_send);
+        MPI_Isend(&stage[(strings_in_tasks[rank] - 1) * NUM_UINTS], NUM_UINTS, MPI_UNSIGNED, (rank + 1) % size, 0, MPI_COMM_WORLD, &req_send[1]);
+
         prev_stages[current_iteration] = (u_int *) calloc(field_size, sizeof(u_int));
         memcpy(prev_stages[current_iteration], stage, field_size * sizeof(u_int));
-        calcNextStage(stage, next_stage, NUM_LINES);
+
+        calcNextStageCenter(stage, next_stage, strings_in_tasks[rank]);
+        MPI_Wait(&req_recv_up, NULL);
+        calcNextStageUp(stage, next_stage, strings_in_tasks[rank], cells_up);
+        MPI_Wait(&req_recv_down, NULL);
+        calcNextStageDown(stage, next_stage, strings_in_tasks[rank], cells_down);
+
         memcpy(stage, next_stage, field_size * sizeof(u_int));
         /*for (int i = 0; i < NUM_LINES; i++) {
             for (int j = 0; j < NUM_UINTS; j++) {
@@ -120,24 +261,35 @@ int main(int argc, char *argv[]) {
             }
             std::cout << std::endl;
         }*/
+        MPI_Waitall(2, req_send, NULL);
+        int period = false;
         for (int i = current_iteration; i > -1; i--) {
-            if (checkPrevStage(prev_stages[i], stage, NUM_LINES)) {
+            if (checkPrevStage(prev_stages[i], stage, strings_in_tasks[rank])) {
                 period = true;
                 break;
             }
         }
+        MPI_Allreduce(&is_global_period, &period, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD);
         current_iteration += 1;
-        std::cout << current_iteration << std::endl;
+        if (rank == 0) {
+            std::cerr << current_iteration << std::endl;
+        }
     }
-
-    if (period) {
-        std::cout << "Periodic" << std::endl;
-    }
-
     for (int i = 0; i < current_iteration; i++) {
         free(prev_stages[i]);
     }
+    free(cells_up);
+    free(cells_down);
+    double end_time = MPI_Wtime();
+
+    if (is_global_period && rank == 0) {
+        std::cout << "Periodic" << std::endl;
+    }
+
     free(stage);
     free(next_stage);
+    free(strings_in_tasks);
+    free(shifts);
+    free(to_send);
     return 0;
 }
